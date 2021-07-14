4871e27a9bf494b60eabb613a6a063dec250e73e
Appledog25 <adititayde2509@gmail.com>
2021-07-14 15:32:43 +0530

    update

---
+++ mod4_solution/js/SpeakGoodBye.js
@@ -0,0 +1,17 @@
+(function (window) {
+
+
+    var byeSpeaker = {
+        name: ["John", "Jen", "Jason", "Jim"]
+    };
+
+
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    }
+
+    window.byeSpeaker = byeSpeaker;
+    var speakWord = "Good Bye";
+
+
+})(window);
---
+++ mod4_solution/js/SpeakHello.js
@@ -0,0 +1,25 @@
+
+(function (window) {
+
+
+    var helloSpeaker = {
+        name: ["Yaakov", "Paula", "Larry", "Laura", "Paul", "Frank"]
+    };
+
+    helloSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    }
+
+    window.helloSpeaker = helloSpeaker;
+    var speakWord = "Hello";
+
+
+})(window);
+
+
+
+
+
+
+
+
